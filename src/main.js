03ca941f084d2eb721f6067239f061869f8290ba
Jarry007 <940615834@qq.com>
2020-10-22 17:48:53 +0800

    :pencil2: antd-vue可以使用了

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@ import { createApp } from 'vue';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import {message} from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
+// import { from } from 'core-js/fn/array';
+// import Element3 from 'element3'
+
 if(process.env.NODE_ENV === 'development') require('@/mock')
-// import store from './'
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App)
+
+const globalDatas = {
+    a:'这是a',
+    b:'this is b',
+    c:'fields c'
+}
+app.config.productionTip = false;
+app.provide('$message',message)
+app.provide('_globalData',globalDatas)
+// app.globalData = globalDatas
+app.config.globalProperties.foo = 'foo'
+app.use(Antd).use(router).use(store).mount('#app')
+
+
--- src/views/login.vue
+++ src/views/login.vue
@@ -8,12 +8,42 @@
 
     <div class="login-input">
       <span class="input-label">图</span>
-      <input v-model="form.password" class="pwd-input" type="text" placeholder="请输入密码" />
-      <span class="input-code" @click="sendCapt">{{showCapt?'获取验证码':`${code} s`}}</span>
+      <input
+        v-model="form.password"
+        class="pwd-input"
+        type="text"
+        placeholder="请输入密码"
+      />
+      <span class="input-code" @click="sendCapt">{{
+        showCapt ? "获取验证码" : `${code} s`
+      }}</span>
     </div>
 
     <div class="login-btn" @click="loginIt">登 录</div>
 
+    <a-button type="primary" @click="messageSend">消息提醒</a-button>
+    <div>
+      <a-select
+        v-model:value="selectValue"
+        @change="selectChange"
+        style="width: 300px"
+      >
+        <a-select-option value="lucy">lucy</a-select-option>
+        <a-select-option value="jack">jack</a-select-option>
+        <a-select-option value="tom">tom</a-select-option>
+        <a-select-option value="shely">shely</a-select-option>
+      </a-select>
+    </div>
+
+    <a-range-picker
+      :show-time="{ format: 'HH:mm' }"
+      format="YYYY-MM-DD HH:mm"
+      :placeholder="['Start Time', 'End Time']"
+      :locale="locale"
+      v-model:value="defaultDate"
+      @change="onChange"
+      @ok="onOk"
+    />
     <!-- <form @submit.prevent="submitLogin">
       <input type="text" v-model="form.username" placeholder="名字" />
       <input type="password" v-model="form.password" placeholder="密码" />
@@ -23,10 +53,12 @@
 </template>
 
 <script>
-import {useRouter} from 'vue-router'
+import { useRouter } from "vue-router";
+import { message } from "ant-design-vue";
 import { checkPhone } from "@/utils/util";
-import { ref, toRefs, reactive } from "vue";
+import { ref, toRefs, reactive, inject } from "vue";
 import { login, getCaptcha, goCheck } from "@/api/login";
+import locale from "ant-design-vue/es/date-picker/locale/zh_CN";
 // import {setTokens} from '@/utils/util'
 import { useStore } from "vuex";
 export default {
@@ -62,10 +94,10 @@ export default {
       } else {
         getCaptcha({ phone: data.form.username })
           .then((res) => {
-            clearInterval(timer)
+            clearInterval(timer);
             data.code = 30;
             showCapt.value = false;
-            data.form.password = res.data.code
+            data.form.password = res.data.code;
             timer = setInterval(() => {
               data.code--;
               if (data.code < 1) {
@@ -88,28 +120,68 @@ export default {
             phone: data.form.username,
           });
           console.log(checkRes);
-            store.commit("setToken", checkRes.data.token);
-            toIndex()
+          store.commit("setToken", checkRes.data.token);
+          toIndex();
         } catch (error) {
-          console.log(error)
+          console.log(error);
           alert(error.msg || "发生了点错误");
         }
       }
     };
-    const router = useRouter()
-    const toIndex = ()=>{
-        
-        router.push({
-            path:'/index'
-        })
-    }
-    return { showCapt, ...toRefs(data), submitLogin, sendCapt, loginIt };
+    const router = useRouter();
+    const toIndex = () => {
+      router.push({
+        path: "/index",
+      });
+    };
+    const msg = inject("$message");
+    const global_ = inject("_globalData");
+    // const { ctx } = getCurrentInstance()
+    const messageSend = () => {
+      console.log(global_);
+      // console.log(ctx)
+      msg.d = "我添加了一个d";
+      msg.a = "reset a";
+
+      msg.error("this is an error message");    
+      message.success("This is a success message");
+    };
+
+    const selectChange = (e) => {
+      console.log(e);
+    };
+    const selectValue = ref("tom");
+
+    // 这里是日期时间选择器相关的
+
+    const onChange = (_, dateString) => {
+      console.log("value, dateString", dateString);
+      data.defaultDate = dateString;
+    };
+
+    const onOk = () => {
+      console.log("数据", data.defaultDate[0], data.defaultDate[1]);
+    };
+    data.defaultDate = ["2020-10-15 11:13", "2020-12-01 11:13"];
+
+    return {
+      showCapt,
+      ...toRefs(data),
+      submitLogin,
+      sendCapt,
+      loginIt,
+      messageSend,
+      selectChange,
+      selectValue,
+      onOk,
+      onChange,
+      locale,
+    };
   },
 };
 </script>
 
 <style scoped>
-
 .input-code {
   font-size: 12px;
   width: 118px;
@@ -138,6 +210,4 @@ export default {
   font-weight: 500;
   margin-top: 100px;
 }
-
-
 </style>
